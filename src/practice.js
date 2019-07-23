67ed1425bf970cdcca5bc08cb962e2bcf2da1ca6
Mantong Zhao <zhaomantong0203@gmail.com>
2019-07-23 08:33:11 -0700

    sql

---
+++ src/practice.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+console.log('connection successful');
+
+
+const searchTerm = 'holo';
+knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .then(result => {
+    console.log(result);
+  });
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+  
+searchByProduceName('holo');
